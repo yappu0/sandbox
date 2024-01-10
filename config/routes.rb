d2ef02ca891c68e39281c255da7f488dfd52b4aa
yappu <59904521+yappu0@users.noreply.github.com>
2024-01-10 10:08:50 +0900

    ユーザー登録できるまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
